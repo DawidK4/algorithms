5821196b2cfc78ebc8be95fb27f47f5a99a25c4f
DawidK4 <s27637@pjwstk.edu.pl>
2025-06-26 20:49:22 +0200

    added selection sort

---
+++ ASD/src/main/java/edu/pja/mas/dkucharski/Sort.java
@@ -0,0 +1,24 @@
+package edu.pja.mas.dkucharski;
+
+public class Sort {
+    /**
+     * Implements the Selection Sort algorithm to sort an array of integers in ascending order.
+     * @param array The array to be sorted.
+     */
+    public static void selectionSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+}
