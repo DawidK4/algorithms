4797df3905b464a8404b322fc49e7b02488c7b8b
DawidK4 <s27637@pjwstk.edu.pl>
2025-06-26 20:36:54 +0200

    added binary search

--- ASD/src/main/java/edu/pja/mas/dkucharski/Search.java
+++ ASD/src/main/java/edu/pja/mas/dkucharski/Search.java
@@ -1,22 +1,71 @@
 package edu.pja.mas.dkucharski;
 
 public class Search {
-    public static int minKJumps(int[] arr, int k) {
-        int n = arr.length;
-        if (n == 0 || k <= 0) return 0;
+    /**
+     * Performs a Jump Search to find the index of a target key in a sorted array.
+     *
+     * @param array The sorted array to search in.
+     * @param key   The value to be searched.
+     * @return The index of the key if found, or -1 if the key is not in the array.
+     */
+    public static int jumpSearch(int[] array, int key) {
+        int n = array.length;
 
-        int[] dp = new int[n];
-        dp[0] = 1;
+        if (n == 0) {
+            return -1;
+        }
 
-        for (int i = 1; i < n; i++) {
-            dp[i] = Integer.MAX_VALUE;
-            for (int j = 0; j < i; j++) {
-                if (i - j <= k && dp[j] != Integer.MAX_VALUE) {
-                    dp[i] = Math.min(dp[i], dp[j] + 1);
-                }
+        int step = (int) Math.floor(Math.sqrt(n));
+
+        int prev = 0;
+
+        while (array[Math.min(step, n) - 1] < key) {
+            prev = step;
+            step += (int) Math.floor(Math.sqrt(n));
+
+            if (prev >= n) {
+                return -1;
             }
         }
 
-        return dp[n - 1] == Integer.MAX_VALUE ? -1 : dp[n - 1];
+        while (array[prev] < key) {
+            prev++;
+            if (prev == Math.min(step, n)) {
+                return -1;
+            }
+        }
+
+        if (array[prev] == key) {
+            return prev;
+        }
+
+        return -1;
+    }
+
+    /**
+     * Performs a Binary Search to find the index of a target key in a sorted array.
+     * @param array - The sorted array to search in.
+     * @param key - The value to be searched.
+     * @return The index of the key if found, or -1 if the key is not in the array.
+     */
+    public static int binarySearch(int[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == key) {
+                return mid;
+            }
+
+            if (array[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
     }
 }
